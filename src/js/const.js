6525233c1e3871e9f6bc51e3234bfe9d376771d3
javyprogrameitor <f.j.gg2@hotmail.com>
2025-02-12 01:41:30 +0100

    Add new songs
    Add songs from one Json because the server Albarregas not functions

--- src/js/const.js
+++ src/js/const.js
@@ -1,3 +1,4 @@
-export const API_URL = "http://informatica.iesalbarregas.com:7008/songs";
+//export const API_URL = "http://informatica.iesalbarregas.com:7008/songs";
 export const API_LOAD = "http://informatica.iesalbarregas.com:7008";
+export const API_URL = "./src/music/songs.json";
 export const FAVORITES_KEY = "favoriteSongs";
